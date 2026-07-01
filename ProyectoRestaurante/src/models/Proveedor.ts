6df7be90b639793b73978f4a80e3488dfcc00859
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-01 10:14:51 -0600

    feat: se ha agregado el interface en el model de proveedor

---
+++ ProyectoRestaurante/src/models/Proveedor.ts
@@ -0,0 +1,7 @@
+export interface proveedor{
+    id_proveedor: number,
+    nombre_proveedor: string,
+    telefono_proveedor: string,
+    correo_proveedor: string,
+    direccion_proveedor: string,
+}
