9d9249859496972581bc1a7e3df10827011d864f
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-01 09:07:18 -0600

    feat:  se ha hecho el interface de pedido

--- ProyectoRestaurante/src/models/Pedido.ts
+++ ProyectoRestaurante/src/models/Pedido.ts
@@ -1,6 +1,13 @@
+import { estado_pedido } from "../enums/Estado_Pedido";
+
 export interface Pedido{
     id_pedido: number; 
     fecha_pedido: Date;
     hora_pedido: string;
-    
+    estado_pedido: estado_pedido,
+    total: number,
+    id_cliente: number,
+    id_mesa: number,
+    id_empleado:number
+
 }
