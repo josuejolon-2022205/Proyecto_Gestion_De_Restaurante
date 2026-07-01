5f159fd19b9088dcac9cfbb89397ee0c1e84c47e
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-01 08:20:17 -0600

    feat: se ha agregado el interface de Empleado.ts

---
+++ ProyectoRestaurante/src/models/Empleado.ts
@@ -0,0 +1,10 @@
+export interface Empleado{
+    id_empleado: number;
+    nombre_empleado: string;
+    apellido_empleado: string;
+    telefono_empleado: number;
+    direccion_empleado: string;
+    salario_empleado: number;
+    id_cargo: number,
+    id_usuario: number
+}
