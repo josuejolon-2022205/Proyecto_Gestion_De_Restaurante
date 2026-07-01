c0ff287276c4db501a780d0173711be0ee17dc0e
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-01 09:16:32 -0600

    feat: se ha agregado el inteface  en el model de producto

---
+++ ProyectoRestaurante/src/models/Producto.ts
@@ -0,0 +1,8 @@
+export interface producto{
+    id_producto: number,
+    nombre_producto: string,
+    descripcion_producto: string,
+    precio: number,
+    //disponibilidad: boolean,
+    id_categoria_producto: number
+}
