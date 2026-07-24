8f820606093c3d21c41b3364606e422b6585fc98
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-24 15:38:05 -0600

    feat: se ha agregado el service de empleado con las funciones para el crud

---
+++ ProyectoRestaurante/src/service/EmpleadoService.ts
@@ -0,0 +1,41 @@
+import { EmpleadoRepository } from "../data/empleadoRepository";
+import { Empleado } from "../models/Empleado";
+
+export class EmpleadoService {
+
+    private repository = new EmpleadoRepository();
+
+    async obtenerEmpleados(): Promise<Empleado[]> {
+        return await this.repository.obtenerEmpleados();
+    }
+
+    async obtenerEmpleadoPorId(id: number): Promise<Empleado | undefined> {
+        return await this.repository.obtenerEmpleadoPorId(id);
+    }
+
+    async guardarEmpleado(empleado: Empleado): Promise<void> {
+        const existe = await this.repository.obtenerEmpleadoPorId(empleado.idEmpleado);
+
+        if (existe) {
+            throw new Error("El ID de empleado ya existe.");
+        }
+
+        await this.repository.guardarEmpleado(empleado);
+    }
+
+    async actualizarEmpleado(empleado: Empleado): Promise<void> {
+        const actualizado = await this.repository.actualizarEmpleado(empleado);
+
+        if (!actualizado) {
+            throw new Error("El empleado no existe.");
+        }
+    }
+
+    async eliminarEmpleado(id: number): Promise<void> {
+        const eliminado = await this.repository.eliminarEmpleado(id);
+
+        if (!eliminado) {
+            throw new Error("El empleado no existe.");
+        }
+    }
+}
