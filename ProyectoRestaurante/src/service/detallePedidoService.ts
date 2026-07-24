6bbda9c86a7775f1a7da4498ae0ed0544b871a37
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-23 18:13:13 -0600

    feat: se ha agregado el service de detalle pedido con su crud

---
+++ ProyectoRestaurante/src/service/detallePedidoService.ts
@@ -0,0 +1,45 @@
+import { DetallePedidoRepository } from "../data/detallePedidoRepository";
+import { DetallePedido } from "../models/DetallePedido";
+
+export class DetallePedidoService {
+
+    private repository = new DetallePedidoRepository();
+
+    async obtenerDetalles(): Promise<DetallePedido[]> {
+        return await this.repository.obtenerDetalles();
+    }
+
+    async obtenerDetallePorId(id: number): Promise<DetallePedido | undefined> {
+        return await this.repository.obtenerDetallePorId(id);
+    }
+
+    async obtenerDetallesPorPedido(idPedido: number): Promise<DetallePedido[]> {
+        return await this.repository.obtenerDetallesPorPedido(idPedido);
+    }
+
+    async guardarDetalle(detalle: DetallePedido): Promise<void> {
+        const existe = await this.repository.obtenerDetallePorId(detalle.idDetallePedido);
+
+        if (existe) {
+            throw new Error("El ID de detalle ya existe.");
+        }
+
+        await this.repository.guardarDetalle(detalle);
+    }
+
+    async actualizarDetalle(detalle: DetallePedido): Promise<void> {
+        const actualizado = await this.repository.actualizarDetalle(detalle);
+
+        if (!actualizado) {
+            throw new Error("El detalle no existe.");
+        }
+    }
+
+    async eliminarDetalle(id: number): Promise<void> {
+        const eliminado = await this.repository.eliminarDetalle(id);
+
+        if (!eliminado) {
+            throw new Error("El detalle no existe.");
+        }
+    }
+}
