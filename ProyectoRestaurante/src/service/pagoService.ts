078236af69a029675ab8f7c0c72e8d2bc16ad94b
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-24 17:12:21 -0600

    feat: se ha agregado el service de pago con su debido crud

---
+++ ProyectoRestaurante/src/service/pagoService.ts
@@ -0,0 +1,45 @@
+import { PagoRepository } from "../data/pagoRepository";
+import { Pago } from "../models/Pago";
+
+export class PagoService {
+
+    private repository = new PagoRepository();
+
+    async obtenerPagos(): Promise<Pago[]> {
+        return await this.repository.obtenerPagos();
+    }
+
+    async obtenerPagoPorId(id: number): Promise<Pago | undefined> {
+        return await this.repository.obtenerPagoPorId(id);
+    }
+
+    async obtenerPagosPorPedido(idPedido: number): Promise<Pago[]> {
+        return await this.repository.obtenerPagosPorPedido(idPedido);
+    }
+
+    async guardarPago(pago: Pago): Promise<void> {
+        const existe = await this.repository.obtenerPagoPorId(pago.idPago);
+
+        if (existe) {
+            throw new Error("El ID de pago ya existe.");
+        }
+
+        await this.repository.guardarPago(pago);
+    }
+
+    async actualizarPago(pago: Pago): Promise<void> {
+        const actualizado = await this.repository.actualizarPago(pago);
+
+        if (!actualizado) {
+            throw new Error("El pago no existe.");
+        }
+    }
+
+    async eliminarPago(id: number): Promise<void> {
+        const eliminado = await this.repository.eliminarPago(id);
+
+        if (!eliminado) {
+            throw new Error("El pago no existe.");
+        }
+    }
+}
