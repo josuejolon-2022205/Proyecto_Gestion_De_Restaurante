57803b65481e39e034121edbbcf985c4704beafd
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-24 18:35:10 -0600

    feat: se ha agregado el router de empleado junto con los endpoints y las rutas a usar en los metodos http

---
+++ ProyectoRestaurante/src/api/empleadoRouter.ts
@@ -0,0 +1,71 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { EmpleadoService } from "../service/EmpleadoService";
+import { ReadBody } from "./readBody";
+import { sendJson } from "./sendJSON";
+import { routeHandler } from "../utils/routeHandler";
+
+const service = new EmpleadoService();
+
+export async function routerEmpleado(req: IncomingMessage, res: ServerResponse) {
+    res.setHeader("Content-Type", "application/json");
+
+    const url = req.url ?? "";
+    const metodo = req.method ?? "";
+    const partes = url.split("/");
+
+    try {
+        if (metodo === "GET" && url === "/empleados") {
+            sendJson(res, 200, await service.obtenerEmpleados());
+            return;
+        }
+
+        if (metodo === "GET" && partes.length === 3 && partes[1] === "empleados") {
+            const id = Number(partes[2]);
+            const empleado = await service.obtenerEmpleadoPorId(id);
+
+            if (!empleado) {
+                sendJson(res, 404, { error: "El id no existe" });
+                return;
+            }
+
+            sendJson(res, 200, empleado);
+            return;
+        }
+
+        if (metodo === "POST" && url === "/empleados") {
+            const body = await ReadBody(req);
+
+            await routeHandler(res, async () => {
+                const e = JSON.parse(body);
+                await service.guardarEmpleado(e);
+                sendJson(res, 201, { mensaje: "Empleado agregado correctamente" });
+            });
+            return;
+        }
+
+        if (metodo === "PUT" && partes.length === 3 && partes[1] === "empleados") {
+            const id = Number(partes[2]);
+            const body = await ReadBody(req);
+
+            await routeHandler(res, async () => {
+                const e = JSON.parse(body);
+                e.idEmpleado = id;
+                await service.actualizarEmpleado(e);
+                sendJson(res, 200, { mensaje: "Empleado actualizado" });
+            });
+            return;
+        }
+
+        if (metodo === "DELETE" && partes.length === 3 && partes[1] === "empleados") {
+            const id = Number(partes[2]);
+            await service.eliminarEmpleado(id);
+            sendJson(res, 200, { mensaje: "Empleado eliminado" });
+            return;
+        }
+
+        sendJson(res, 404, { error: "No se encontro la ruta" });
+
+    } catch (error) {
+        sendJson(res, 500, { error: (error as Error).message });
+    }
+}
