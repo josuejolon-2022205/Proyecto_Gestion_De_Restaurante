c09505e1f3c0fb6f1a9ec6dd77fbb26d854b43a1
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-24 20:39:17 -0600

    feat: se han agregado las importaciones/enrutamiento para los routers en server.ts

--- ProyectoRestaurante/src/api/server.ts
+++ ProyectoRestaurante/src/api/server.ts
@@ -1,7 +1,75 @@
-import * as http from "http"
+import * as http from "http";
+import { routerCargo } from "./cargoRouter";
+import { routerEmpleado } from "./empleadoRouter";
+import { routerIngrediente } from "./ingredienteRouter";
+import { routerProductoIngrediente } from "./productoIngredienteRouter";
+import { routerCategoriaProducto } from "./categoriaProductoRouter";
+import { routerDetallePedido } from "./detallePedidoRouter";
+import { clienteRouter } from "./clienteRouter";
+import { routerUsuario } from "./usuarioRouter";
+import { routerReserva } from "./reservaRouter";
+import { routerFactura } from "./facturaRouter";
+import { routerMovimientoInventario } from "./movimientoInventarioRouter";
+import { routerProducto } from "./productoRouter";
+import { routerRol } from "./rolRouter";
+import { routerPedido } from "./pedidoRouter";
+import { routerMesa } from "./mesaRouter";
+import { routerProveedor } from "./proveedorRouter";
+import { routerPago } from "./pagoRouter";
 
-const port = 3000
+export const server = http.createServer(async (req, res) => {
+    await routerCargo(req, res);
+    if (res.headersSent) return;
 
-const server = http.createServer(async(req, res) => {
+    await routerEmpleado(req, res);
+    if (res.headersSent) return;
 
-}) 
+    await routerIngrediente(req, res);
+    if (res.headersSent) return;
+
+    await routerProductoIngrediente(req, res);
+    if (res.headersSent) return;
+
+    await routerCategoriaProducto(req, res);
+    if (res.headersSent) return;
+
+    await routerDetallePedido(req, res);
+    if (res.headersSent) return;
+
+    await clienteRouter(req, res);
+    if (res.headersSent) return;
+
+    await routerUsuario(req, res);
+    if (res.headersSent) return;
+
+    await routerReserva(req, res);
+    if (res.headersSent) return;
+
+    await routerFactura(req, res);
+    if (res.headersSent) return;
+
+    await routerMovimientoInventario(req, res);
+    if (res.headersSent) return;
+
+    await routerProducto(req, res);
+    if (res.headersSent) return;
+
+    await routerRol(req, res);
+    if (res.headersSent) return;
+
+    await routerPedido(req, res);
+    if (res.headersSent) return;
+
+    await routerMesa(req, res);
+    if (res.headersSent) return;
+
+    await routerProveedor(req, res);
+    if (res.headersSent) return;
+
+    await routerPago(req, res);
+    if (res.headersSent) return;
+
+    // Si ningún router respondió, enviar 404 global
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "No se encontro la ruta" }));
+});
