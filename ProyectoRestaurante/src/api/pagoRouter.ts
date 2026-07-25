e5d3bf077233ba383477f35ead77a69e0261b92c
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-24 18:59:39 -0600

    feat: se ha agregado el router de pago junto con los endpoints y las rutas a usar en los metodos http

---
+++ ProyectoRestaurante/src/api/pagoRouter.ts
@@ -0,0 +1,51 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { PagoService } from "../service/pagoService";
+import { ReadBody } from "./readBody";
+import { sendJson } from "./sendJSON";
+import { routeHandler } from "../utils/routeHandler";
+
+const service = new PagoService();
+
+export async function routerPago(req: IncomingMessage, res: ServerResponse) {
+    res.setHeader("Content-Type", "application/json");
+
+    const url = req.url ?? "";
+    const metodo = req.method ?? "";
+    const partes = url.split("/");
+
+    try {
+        if (metodo === "GET" && url === "/pagos") {
+            sendJson(res, 200, await service.obtenerPagos());
+            return;
+        }
+
+        if (metodo === "GET" && partes.length === 3 && partes[1] === "pagos") {
+            const id = Number(partes[2]);
+            const pago = await service.obtenerPagoPorId(id);
+
+            if (!pago) {
+                sendJson(res, 404, { error: "El id no existe" });
+                return;
+            }
+
+            sendJson(res, 200, pago);
+            return;
+        }
+
+        if (metodo === "POST" && url === "/pagos") {
+            const body = await ReadBody(req);
+
+            await routeHandler(res, async () => {
+                const p = JSON.parse(body);
+                await service.guardarPago(p);
+                sendJson(res, 201, { mensaje: "Pago registrado correctamente" });
+            });
+            return;
+        }
+
+        sendJson(res, 404, { error: "No se encontro la ruta" });
+
+    } catch (error) {
+        sendJson(res, 500, { error: (error as Error).message });
+    }
+}
