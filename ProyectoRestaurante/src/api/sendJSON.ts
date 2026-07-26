f13f37e4248b6c031f3ce75f2b9979e4b4fae15b
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-25 21:53:02 -0600

    refactor: se cambio la funcion withTryCatch que era innecesario para las funciones sincrones

--- ProyectoRestaurante/src/api/sendJSON.ts
+++ ProyectoRestaurante/src/api/sendJSON.ts
@@ -1,12 +1,8 @@
 import { ServerResponse } from "http";
-import { withTryCatch } from "../utils/withTryCatch";
-function _sendJson(res: ServerResponse, status: number, data: unknown): void {
+
+export function sendJson(res: ServerResponse, status: number, data: unknown): void {
     res.writeHead(status, {
         "Content-Type": "application/json"
     });
     res.end(JSON.stringify(data));
 }
-
-export const sendJson = withTryCatch(
-    _sendJson, undefined,"Error al enviar respuesta JSON."
-);
