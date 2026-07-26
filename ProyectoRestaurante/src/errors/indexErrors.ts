5551da56b59d4c562aed2b6d299cfe1f249b69fe
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-26 01:25:09 -0600

    feat: se agregaron las exportaciones de los errores para manejarlas en un solo archivo en indexErrors.ts

---
+++ ProyectoRestaurante/src/errors/indexErrors.ts
@@ -0,0 +1,5 @@
+export { AppError } from "./AppError";
+export { ValidationError } from "./ValidationError";
+export { NotFoundError } from "./NotFoundError";
+export { ConflictError } from "./ConflictError";
+export { DatabaseError } from "./DatabaseError";
