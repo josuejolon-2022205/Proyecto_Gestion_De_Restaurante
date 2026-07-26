12f30a758c39401ffcf6c36987a9c4a828833ba0
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-25 21:58:07 -0600

    refactor: se ha cambiado el withTryCatch para tipar solo async y se agrego otra funcion

--- ProyectoRestaurante/src/utils/withTryCatch.ts
+++ ProyectoRestaurante/src/utils/withTryCatch.ts
@@ -1,14 +1,28 @@
 export function withTryCatch<TArgs extends any[], TReturn>(
-  fn: (...args: TArgs) => TReturn | Promise<TReturn>,
-  defaultValue: TReturn,
-  errorMsg: string
+    fn: (...args: TArgs) => Promise<TReturn>,
+    defaultValue: TReturn,
+    errorMsg: string
 ): (...args: TArgs) => Promise<TReturn> {
-  return async (...args: TArgs): Promise<TReturn> => {
-    try {
-      return await fn(...args);
-    } catch (error) {
-      console.log(errorMsg);
-      return defaultValue;
-    }
-  };
+    return async (...args: TArgs): Promise<TReturn> => {
+        try {
+            return await fn(...args);
+        } catch (error) {
+            console.log(errorMsg);
+            return defaultValue;
+        }
+    };
+}
+
+export function withTryCatchThrow<TArgs extends any[], TReturn>(
+    fn: (...args: TArgs) => Promise<TReturn>,
+    errorMsg: string
+): (...args: TArgs) => Promise<TReturn> {
+    return async (...args: TArgs): Promise<TReturn> => {
+        try {
+            return await fn(...args);
+        } catch (error) {
+            console.log(errorMsg);
+            throw error;
+        }
+    };
 }
